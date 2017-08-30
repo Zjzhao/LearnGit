14a66f69bad5d7dee06ece45f624dd7e1906437c
zhaozhangjie <zhaozhangjie@outlook.com>
2017-08-30 14:27:56 +0800

    new project

---
+++ LearnGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnGit
+//
+//  Created by 赵张杰 on 2017/8/30.
+//  Copyright © 2017年 zhaozhangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnGit
+//
+//  Created by 赵张杰 on 2017/8/30.
+//  Copyright © 2017年 zhaozhangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
